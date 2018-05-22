3cd8c0db397060301de125232634eda02e4a2520
seancolyer <sean.colyer@virtru.com>
2018-05-22 14:11:35 -0400

    Cleanup

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const voltron = require('./lib/voltron');
 module.exports = async function(opts) {
   const packageJson = voltron.findPackageJson(opts.cwd);
   const extNames = voltron.getExtensionNames(packageJson, opts);
-  console.log('EXTENSIONS' + extNames);
+  console.log(`Running voltron for: ${extNames}`);
 
   await voltron.installDevDeps(extNames, opts.cwd);
   const configs = voltron.getConfigs(extNames, opts.cwd);
   await voltron.buildExtensions(configs, opts.buildOpts);
   const manifest = await voltron.updateManifest(configs, opts.manifest);
-  console.log('complete');
+  console.log('Voltron complete');
   return manifest;
 };
